5fe38455fc376c1277f2c3694920c7652ccc068a
Yuchen Guo <Yuchen.Guo@wri.org>
2024-01-30 20:35:25 +0800

    update

--- add_geo-information.py
+++ add_geo-information.py
@@ -45,7 +45,7 @@ api_key = f[0]
 GOOGLE_MAPS_API_KEY = api_key
 
 
-######省
+######如何处理省级别的shapefile
 shp_province = gpd.read_file('C:/Users/Yuchen.Guo/OneDrive/WRI/github/shpfile/2020年_CTAmap_1.12版/2020年省级/2020年省级.shp')
 shp_province.set_index('省',inplace = True)
 province = pd.read_excel('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/第七次人口普查分县数据_1128.xlsx',sheet_name = 'new_data_province' )
@@ -53,33 +53,25 @@ province.set_index('ID',inplace = True)
 shp_province_new = shp_province.reindex(province['省'])
 shp_province_new['ID'] = province.index 
 shp_province_new.reset_index().dropna(subset=['geometry']).to_file('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/China_province.shp', index = False,driver='ESRI Shapefile',encoding='utf-8')
-
-
 a = province.index.values
 b= shp_province_new.reset_index().dropna(subset=['geometry'])['ID'].values
 empty = set(a) - set(b)
 
-######市
+######如何处理市级别的shapefile
 shp_city = gpd.read_file('C:/Users/Yuchen.Guo/OneDrive/WRI/github/shpfile/2020年_CTAmap_1.12版/2020年地级/2020年地级.shp')
 shp_city['all_name'] = shp_city['省级'] + shp_city['地名']
 shp_city.set_index('all_name',inplace = True)
-
 city = pd.read_excel('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/第七次人口普查分县数据_1128.xlsx',sheet_name = 'new_data_city' )
 city.set_index('ID',inplace = True)
 city['all_name'] = city['省']+city['市']
-
-
 shp_city_new = shp_city.reindex(city['all_name'])
 shp_city_new['ID'] = city.index 
 shp_city_new.reset_index().dropna(subset=['geometry']).to_file('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/China_city.shp', index = False,driver='ESRI Shapefile',encoding='utf-8')
-
 a = city.index.values
 b= shp_city_new.reset_index().dropna(subset=['geometry'])['ID'].values
 empty = set(a) - set(b)
 
-
-######县
-
+######如何处理县级别shapfile
 shp_county = gpd.read_file('C:/Users/Yuchen.Guo/OneDrive/WRI/github/shpfile/2020年_CTAmap_1.12版/2020年县级/2020年县级.shp')
 shp_county['all_name'] = shp_county['省级'] +shp_county['地级']+ shp_county['地名']
 shp_county.set_index('all_name',inplace = True)
@@ -97,8 +89,6 @@ a = county.index.values
 b = shp_county_new.reset_index().dropna(subset=['geometry'])['ID'].values
 empty = set(a) - set(b)
 
-
-
 address_list = county.loc[list(empty)]['all_name']
 
 exists =  os.path.exists('C:/Users/Yuchen.Guo/OneDrive/WRI/github/shpfile/datamissingname.csv')
@@ -108,6 +98,7 @@ if exists:
     datamissingname = pd.read_csv('C:/Users/Yuchen.Guo/OneDrive/WRI/github/shpfile/datamissingname.csv').set_index('ID')
 
 else:
+    ## 寻找出没有被识别出来的县区
     LON, LAT, Full_name = geocode_address(address_list, api_key)
 
     datamissingname = pd.DataFrame({'lon':LON,'lat':LAT,'Full_name':Full_name,'all_name':address_list,'ID':address_list.index}).set_index('ID')
@@ -115,7 +106,6 @@ else:
     datamissingname.to_csv('C:/Users/Yuchen.Guo/OneDrive/WRI/github/shpfile/datamissingname.csv')
 
 points = [Point(lon,lat)for lon,lat in zip(datamissingname['lon'],datamissingname['lat'])]
-
 
 datamissing = pd.DataFrame()
 
@@ -128,7 +118,6 @@ for pp,idd in zip(points,address_list.index):
 datamissing.drop(columns=['all_name'], inplace=True)
 
 datamissing = pd.merge(datamissingname, datamissing, left_index=True, right_index=True)
-
 gdf = datamissing.reset_index().dropna(subset=['geometry']).rename(columns = {'index':'ID'})
 gpd.GeoDataFrame(gdf, geometry='geometry').to_file('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/China_county_need_further_clean.shp',driver='ESRI Shapefile',encoding='utf-8')
 
@@ -145,12 +134,10 @@ xiid = CN & CC
 
 xiID = [China_county_need_further_clean[China_county_need_further_clean['区划码'] == ii].index[0] for ii in xiid]
 
-
 uiID = [China_county_need_further_clean[China_county_need_further_clean['区划码'] == ii].index[0] for ii in iiid]
 
 
 China_county_ = China_county_need_further_clean.loc[uiID][China_county.columns[:]]
-
 
 China_county_need_further_clean.loc[xiID][China_county.columns[:]]
 
@@ -197,7 +184,7 @@ for province, dataframex in county.loc[list(noboundry_cen)].sort_values(by='省'
         dataframe = dataframe.rename({0:ID})
         Dataframe = pd.concat([Dataframe,dataframe])
         
-        
+############## 手动处理部分开始       
 data = {
     "ID": [838, 833, 830, 22, 2334],
     "Description": [
@@ -208,61 +195,32 @@ data = {
         "海南省三沙市南沙区"
     ]
 }
-
 # Create a DataFrame
 df = pd.DataFrame(data)
-
 shp_cty = shp_county.loc[df['Description']].reset_index()[China_county.columns[:]]
 shp_cty['ID'] = df['Description'].index
 #shp_cty = #shp_county.loc[[838, 833, 830, 22, 2334]]
 shp_cty.set_index('ID').reset_index().to_file('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/China_county_v2_added.shp',index = False,driver='ESRI Shapefile',encoding='utf-8')
-
 ddd = Dataframe[Dataframe['status'] != 'None'].drop(index = 873)
-
 dddd = Dataframe[Dataframe['status'] != 'None'].dropna()
-
-
 shp_cty = shp_county.loc[ddd['status']].reset_index()[China_county.columns[:]]
 shp_cty['ID'] = ddd['status'].index
-
-
-
 final_shapefile = pd.concat([shp_cty.set_index('ID'),China_county_final])
-
-
- 
-
 empty_shapefile = shp_county.set_index('区划码').loc[list(set(shp_county['区划码'])-set(final_shapefile['区划码']))]
-
-
 es = empty_shapefile.loc[['310109']].reset_index()
-
 es['all_name'] = final_shapefile.loc[[832]]['all_name'].values[0]
-
-
 final_shapefile.loc[[832]] = es[final_shapefile.columns].values
-
 final_shapefile.reset_index().to_file('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/China_county_v3.shp',index = False,driver='ESRI Shapefile',encoding='utf-8')
+############## 手动处理部分结束
 
-for iiddd, value in empty_shapefile[:].iterrows():
-    print(value['省级'],value['地级'],value['地名']) 
-    p_county = county[county['省'] == value['省级']]
-    p_county[p_county['市'] == value['地级']]
-    
-    
-    
+
+#输入Unique INDEX   
 need_v_shape = shp_county.set_index('区划码').loc[np.array(county[county['年份'] == 'F2020'].index).astype(str)]
-
 need_v_shape['ID'] = need_v_shape.index
-
 county[county['年份'] == 'F2020']['all_name']
-
 need_v_shape['all_name'] = county[county['年份'] == 'F2020']['all_name'].values
-    
 need_v_shape_ = need_v_shape.reset_index().set_index('ID')[final_shapefile.columns]
-
 final_shapefile_ = pd.concat([need_v_shape_,final_shapefile])
-
 final_shapefile_.reset_index().to_file('C:/Users/Yuchen.Guo/OneDrive - World Resources Institute/RDI/Data Team/7th CENSUS/China_county_v3.shp',index = False,driver='ESRI Shapefile',encoding='utf-8')
 
 
